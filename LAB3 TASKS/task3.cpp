9a9c498619b49552dfcdb2c7c236880ac9eff924
AsharAdnan0606 <oashar866@gmail.com>
2025-02-13 01:06:08 +0500

    Update task3.cpp

--- LAB3 TASKS/task3.cpp
+++ LAB3 TASKS/task3.cpp
@@ -1,1 +1,30 @@
-
+#include<iostream>
+#include<string>
+using namespace std;
+class Glass{	
+	public:
+		int liquidLevel;
+		
+		void drink(int liquidDrank){
+			liquidLevel-=liquidDrank;
+		}
+		
+		void refill(){
+			if(liquidLevel<100){
+				liquidLevel=200;
+				cout<<"Liquid Refilled "<<"(Liquid Level: "<<liquidLevel<<" )"<<endl;
+			}else{
+				cout<<"Liquid Level Remaining : "<<liquidLevel<<endl;
+			}
+		}
+};
+int main(int argc , char *argv[]){
+	Glass g1;
+	g1.liquidLevel=200;
+	int count=0;
+	for(int i=0 ; i<argc-1 ; i++){
+	g1.drink(stoi(argv[++count]));
+	g1.refill();	
+ }			
+	return 0;
+}
